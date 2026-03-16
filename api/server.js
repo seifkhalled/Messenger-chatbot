c5ea44f134918f923b6aeba6ccfba1bd87417c4d
seifkhalled <Seifkhalled684@gmail.com>
2026-03-16 10:07:45 +0200

    Fix Vercel deployment configuration

--- api/server.js
+++ api/server.js
@@ -18,7 +18,7 @@ initWebRoutes(app);
 
 // For local development
 let port = process.env.PORT || 3000;
-if (process.env.NODE_ENV !== 'vercel') {
+if (process.env.NODE_ENV !== 'production') {
     app.listen(port, () => {
         console.log("App is running at the port: " + port);
     });
